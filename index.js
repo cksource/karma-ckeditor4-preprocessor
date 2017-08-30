83cfd632302ff5c83d4206c223d0ac2c48dcdfdc
Krzysztof Krztoń <k.krzton@cksource.com>
2017-08-30 13:54:21 +0200

    Includes test and fixture full and relative paths. Added docs to 'index.js'.

--- index.js
+++ index.js
@@ -9,31 +9,91 @@ const fs = require( 'fs' );
 const path = require( 'path' );
 const meta = require( './src/meta' );
 
-function getHtmlFixture( filePath ) {
+/**
+ * Returns relative path starting from a given folder based on absolute file path:
+ *
+ * 		filePath: '/Workspace/CKSource/CKEditor4/ckeditor/ckeditor-dev/tests/core/ckeditor/ckeditor.js'
+ * 		start: 'tests'
+ * 		returns: 'tests/core/ckeditor/ckeditor.js'
+ *
+ * @param {String} filePath Absolute path to a file.
+ * @param {String} start Start folder from which relative path should start.
+ * @returns {String|null} Relative path or null.
+ */
+function getRelativePath( filePath, start ) {
+	start = `${ path.sep }${ start }${ path.sep }`;
+	if ( filePath.indexOf( start ) !== -1 ) {
+		return path.join( start, filePath.split( start ).pop() ).substr( 1 );
+	}
+	return null;
+}
+
+/**
+ * Returns test name based on the absolute test file path. By default CKEditor tests are placed in `tests` directory.
+ * This directory is treated as a start of the test name:
+ *
+ * 		filePath: '/Workspace/CKSource/CKEditor4/ckeditor/ckeditor-dev/tests/core/ckeditor/ckeditor.js'
+ * 		returns: 'tests/core/ckeditor/ckeditor'
+ *
+ * @param {String} filePath Absolute path to a test file.
+ * @returns {String|null} Test name or null.
+ */
+function getTestName( filePath ) {
+	const testRelativePath = getRelativePath( filePath, 'tests' );
+	return testRelativePath ? testRelativePath.replace( /\.js$/, '' ) : null;
+}
+
+/**
+ * Returns test file info containing absolute and relative file paths. The relative path starts from default
+ * CKEditor tests folder which is `tests`.
+ *
+ * @param {String} filePath Absolute path to a test file.
+ * @returns {Object} Object containing relative and absolute file paths.
+ * @returns {String} return.path Relative path to a test file.
+ * @returns {String} return.fullpath Absolute path to a test file.
+ */
+function getTestFileInfo( filePath ) {
+	return {
+		path: getRelativePath( filePath, 'tests' ),
+		fullpath: filePath
+	};
+}
+
+/**
+ * Returns test fixture file info containing absolute and relative file paths. The relative path starts from default
+ * CKEditor tests folder which is `tests`.
+ *
+ * @param {String} filePath Absolute path to a test file.
+ * @returns {Object} Object containing relative and absolute file paths.
+ * @returns {String} return.path Relative path to a fixture file.
+ * @returns {String} return.fullpath Absolute path to a fixture file.
+ */
+function getFixtureFileInfo( filePath ) {
 	const fixturePath = filePath.replace( /\.js$/, '.html' );
 	if ( filePath && fs.existsSync( fixturePath ) ) {
-		return fixturePath;
+		return {
+			path: getRelativePath( fixturePath, 'tests' ),
+			fullpath: filePath
+		};
 	}
 	return null;
 }
 
-function getTestName( filePath ) {
-	const testsDir = `${ path.sep }tests${ path.sep }`;
-	if ( filePath.indexOf( testsDir ) !== -1 ) {
-		return path.join( testsDir, filePath.split( testsDir ).pop() );
-	}
-	return null;
-}
-
+/**
+ * Creates preprocessor which converts bender tags (e.g. `/* bender-tags: editor *\/`) into JavaScript object which then
+ * can be accessed by running tests. It also contains test name and test and fixture file paths.
+ *
+ * @returns {Function} Preprocessor function.
+ */
 function createCKEditor4Preprocessor() {
 
 	return ( content, file, done ) => {
 		const tags = meta.parse( content ),
-			htmlFixture = getHtmlFixture( file.path );
+			htmlFixture = getFixtureFileInfo( file.path );
 
 		tags.test = {
 			name: getTestName( file.path ),
-			file: file.path
+			file: getTestFileInfo( file.path )
 		};
 
 		if ( htmlFixture ) {
